9af8321ef35d5adedfd8606b039898143886cf92
natumsol <natumsol@gmail.com>
2015-10-27 23:08:37 +0800

    add inde.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '数据库加密实验' });
 });
 
 module.exports = router;
